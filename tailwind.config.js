4e1e1978cd7b61d67e50a4b505d5d958af28245f
cravinx <unegbuprecious25@gmail.com>
2024-10-07 02:45:52 +0200

    First Commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+
+    fontFamily:{
+      "Poetsen-One": ["Poetsen One"],
+      'Platypi': ['Platypi']
+    },
+    extend: {},
+  },
+  plugins: [],
+}
